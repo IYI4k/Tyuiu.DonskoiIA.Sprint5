738b823ccb638f213ef068b06702fdb712367e46
IYI4k <IYI4k@yandex.ru>
2024-10-21 17:55:02 +0500

    Task5

--- Tyuiu.DonskoiIA.Sprint5.Task5.V30.Lib/DataService.cs
+++ Tyuiu.DonskoiIA.Sprint5.Task5.V30.Lib/DataService.cs
@@ -12,7 +12,6 @@ namespace Tyuiu.DonskoiIA.Sprint5.Task5.V30.Lib
 {
     public class DataService : ISprint5Task5V30
     {
-
         public double LoadFromDataFile(string path)
         {
             double ans = double.MinValue;
